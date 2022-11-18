6131fde18900cd246b3f34a3117386a41729ea2c
sirflyingv <andthenyouadd@gmail.com>
2022-11-18 13:10:30 +0300

    setup npm

---
+++ Makefile
@@ -0,0 +1,8 @@
+install: # basic install
+	npm ci
+
+brain-games: 
+	node bin/brain-games.js
+
+publish: # dry publish
+	npm publish --dry-run 
---
+++ bin/brain-games.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// ⬆ defines default interpretator of this program as executable file
+// 🙄🙄🙄 why package is named @hexlet/code but registered as brain-games
+
+console.log("Welcome to the Brain Games!");
