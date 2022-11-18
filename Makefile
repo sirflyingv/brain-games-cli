a46a374e75a405b02ff6bdd15a8e30b537b2b23d
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-18 18:47:51 +0300

    eslit added

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ brain-games:
 
 publish: # dry publish
 	npm publish --dry-run 
+
+lint:
+	npx eslint .
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greet = function () {
-  const name = readlineSync.question("May I have your name? ");
+export const greet = () => {
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
