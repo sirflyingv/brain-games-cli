521df3a51b8d73ae34df4a770f7e7c3b1712c0d5
sirflyingv <andthenyouadd@gmail.com>
2022-11-18 14:49:22 +0300

    readlineSync added

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 // ⬆ defines default interpretator of this program as executable file
-// 🙄🙄🙄 why package is named @hexlet/code but registered as brain-games
+import { greet } from "../src/cli.js";
 
 console.log("Welcome to the Brain Games!");
+
+greet();
