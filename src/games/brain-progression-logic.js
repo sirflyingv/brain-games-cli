7580d9ca405454d6ebd8e25dcd35f4a0cd61cc1f
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-20 00:10:19 +0300

    brain-progression game

---
+++ src/games/brain-progression-logic.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+export const rulesStr = 'What number is missing in the progression?';
+
+export const gameLogicProgression = (valueGenerator) => {
+  const progLength = valueGenerator(5) + 5;
+  const progStart = valueGenerator(15);
+  const progStep = valueGenerator(5);
+  const secretIndex = valueGenerator(progLength) - 1;
+  const progression = [progStart];
+
+  for (let i = 2; i <= progLength; i += 1) {
+    progression.push(progression[i - 2] + progStep);
+  }
+
+  const questionStr = progression
+    .map((el, i) => (i === secretIndex ? '..' : el))
+    .join(' ');
+
+  console.log(`Question: ${questionStr}`);
+
+  const expectedAnswer = progression[secretIndex];
+  const userAnswer = readlineSync.question('Your answer: ').trim();
+  const isCorrect = Number(userAnswer) === expectedAnswer;
+
+  return {
+    isCorrect,
+    userAnswer,
+    expectedAnswer,
+  };
+};
