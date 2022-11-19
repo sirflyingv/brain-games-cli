d34565c1700ea5e3179ed4be84c8bcfb79817e0d
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-19 15:20:27 +0300

    brain-calc game

---
+++ src/games/brain-calc-logic.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export const rulesStr = 'What is the result of the expression?';
+
+export const gameLogicCalc = (valueGenerator) => {
+  const num1 = valueGenerator();
+  const num2 = valueGenerator();
+
+  let expression = '';
+  let expectedAnswer = 0;
+  const operatorSeed = valueGenerator();
+
+  if (operatorSeed <= 7) {
+    expression = `${num1} + ${num2}`;
+    expectedAnswer = num1 + num2;
+  }
+  if (operatorSeed > 7 && operatorSeed < 14) {
+    expression = `${num1} - ${num2}`;
+    expectedAnswer = num1 - num2;
+  }
+  if (operatorSeed >= 14) {
+    expression = `${num1} * ${num2}`;
+    expectedAnswer = num1 * num2;
+  }
+
+  console.log(`Question: ${expression}`);
+  const userAnswer = Number(readlineSync.question('Your answer: ').trim());
+  const isCorrect = userAnswer === expectedAnswer;
+
+  return {
+    isCorrect,
+    userAnswer,
+    expectedAnswer,
+  };
+};
