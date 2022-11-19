d45e8e6097e6327886dc378c4b70773de5c97828
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-19 15:09:09 +0300

    clean comments

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// import readlineSync from 'readline-sync';
 import runGame from '../src/index.js';
 import { gameLogicEven, rulesStr } from '../src/games/brain-even-logic.js';
 
