705034fa03a9dcc84490174a53b51ccf8a46cf6a
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-20 00:09:47 +0300

    fix

--- src/games/brain-even-logic.js
+++ src/games/brain-even-logic.js
@@ -4,6 +4,7 @@ export const rulesStr = 'Answer "yes" if number is even, otherise answer "no".';
 
 export const gameLogicEven = (valueGenerator) => {
   const number = valueGenerator(20);
+
   console.log(`Question: ${number}`);
 
   const expectedAnswer = number % 2 === 0 ? 'yes' : 'no';
