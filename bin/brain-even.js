455693473f29840c3964bae471d05a3d0aa091e8
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-19 14:36:08 +0300

    refactor: separate game logic from bin

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,8 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 import runGame from '../src/index.js';
-
-const rulesStr = 'Answer "yes" if number is even, otherise answer "no".';
-
-function gameLogicEven(valueGenerator) {
-  const number = valueGenerator();
-  console.log(`Question: ${number}`);
-
-  const expectedAnswer = number % 2 === 0 ? 'yes' : 'no';
-  const userAnswer = readlineSync.question('Your answer: ').trim();
-  const isCorrect = userAnswer === expectedAnswer;
-
-  return {
-    isCorrect,
-    userAnswer,
-    expectedAnswer,
-  };
-}
+import { gameLogicEven, rulesStr } from '../src/games/brain-even-logic.js';
 
 runGame(gameLogicEven, rulesStr);
 
