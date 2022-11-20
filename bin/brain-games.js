7bea9b033d053ab315df80b145278c3d1d058f78
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-11-20 17:01:40 +0300

    linting fixes

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // ⬆ defines default interpretator of this program as executable file
-import { greet } from '../src/cli.js';
+import greet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greet();
